a148c2672b0aba9d8d52d97f9df12f89dced48d6
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-04-08 17:20:30 +0500

    Login

---
+++ MoviePortal/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MoviePortal.Models.Account;
+
+namespace MoviePortal.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+
+        public AccountController(SignInManager<User> signInManager, UserManager<User> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginDTO model)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, false, false);
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("LoginFail", "UserName or password is wrong");
+                return View(model);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
--- MoviePortal/Program.cs
+++ MoviePortal/Program.cs
@@ -3,17 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MoviePortal.Context;
+using MoviePortal.Models.Account;
 
 namespace MoviePortal
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            try
+            {
+                var context = services.GetRequiredService<MoviePortalContext>();
+                var userManager = services.GetRequiredService<UserManager<User>>();
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                await context.Database.MigrateAsync();
+
+
+
+                logger.LogInformation("Migrate success!");
+
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message);
+            }
+
+
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
